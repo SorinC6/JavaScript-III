0776352499822d9c6ef443314432c5fa554900c1
Sorin Chis <kish.sorin@yahoo.com>
2019-01-23 14:19:51 +0200

    added answer to : the for principles of this keyword

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding  - when we use din as a global scobe, you cand bine any variabile, object to the window ex: window.age=3; .. or you can console.log(this);
+* 2. Implicit Binding - when invok a function by a dot , instead of writing the object we can adreess like  this.function not object.function
+* 3. New Binding - when we create a object with a contruction function , the object its created and returned without writing the return statement
+* 4. Explicit Binding  - when call or apply is used on a object , when we want to take a method from another object. ex: person.spleak.call(child)
 *
 * write out a code example of each explanation above
 */
